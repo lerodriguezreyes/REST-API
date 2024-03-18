8d518be47571bb849d668d8dab91fd5bc4d24111
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-03-18 16:32:48 -0400

    after setup by Dustin

--- server/app.js
+++ server/app.js
@@ -1,27 +1,31 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors')
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var authRouter = require('./routes/auth')
+var apiRouter = require('./routes/api')
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
-    cors({
-      origin: [process.env.REACT_APP_URI],
-    })
-  );
+  cors({
+    origin: [process.env.REACT_APP_URI],
+  })
+);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use('/auth', authRouter);
+app.use('/api', apiRouter)
 
 module.exports = app;
