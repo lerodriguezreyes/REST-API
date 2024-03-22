7dc0dfaa5d18978ef1e2785f74ddd0405a68c38c
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-03-22 19:09:55 -0400

    REST API ready!

--- server/app.js
+++ server/app.js
@@ -4,17 +4,14 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var cors = require("cors");
 
-var indexRouter = require("./routes/index");
+// var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
-var authRouter = require('./routes/auth')
-var apiRouter = require('./routes/api')
-var studentsRouter = require('../server/routes/students')
-var cohortsRouter = require('../server/routes/cohorts')
+var authRouter = require('./routes/auth');
+var apiRouter = require('./routes/api');
+var studentsRouter = require('../server/routes/students');
+var cohortsRouter = require('../server/routes/cohorts');
 
 const mongoose = require("mongoose");
-
-// const Cohort = require("./models/Cohort.model");
-// const Student = require("./models/Student.model");
  
 const app = express();
 
@@ -30,51 +27,23 @@ app.use(
   })
 );
 
-app.use("/", indexRouter);
+// app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use('/auth', authRouter);
 app.use('/api', apiRouter);
 
-app.use('/students', studentsRouter)
-app.use('/cohorts', cohortsRouter)
-
-
-// THESE ARE ALL THE STATIC ROUTES. I THINK WE CAN ERASE THEM AND ONLY LEAVE THE HTML ROUTE FOR THE DOCS - LUIS E.
-//Cohort:
-// app.get("/cohorts", (req, res) => {
-//   Cohort.find({})
-//     .then((cohorts) => {
-//       console.log("Retrieved cohorts ->", cohorts);
-//       res.json(cohorts);
-//     })
-//     .catch((error) => {
-//       console.error("Error while retrieving cohorts ->", error);
-//       res.status(500).send({ error: "Failed to retrieve cohorts" });
-//     });
-// });
-
-// //Students:
-// app.get("/students", (req, res) => {
-//   Student.find({})
-//     .then((students) => {
-//       console.log("Retrieved students ->", students);
-//       res.json(students);
-//     })
-//     .catch((error) => {
-//       console.error("Error while retrieving students ->", error);
-//       res.status(500).send({ error: "Failed to retrieve students" });
-//     });
-// });
+app.use('/students', studentsRouter);
+app.use('/cohorts', cohortsRouter);
 
 // docs route:
 app.get('/docs', (request, response, next) => {
-  console.log(request);
-  response.sendFile(__dirname + '/views/docs.html');
+    console.log(request);
+    response.sendFile(__dirname + '/views/docs.html');
 });
 
 // THIS IS THE MONGOOSE CONNECTION, DO NOT ERASE
 mongoose
-  .connect("mongodb://127.0.0.1:27017/cohort-tools-api")
+  .connect(process.env.MONGODB_URI)  //PONER EL NOMBRE DE LA VARIABLE QUE ESTA EN .ENV
   .then(x => console.log(`Connected to Database: "${x.connections[0].name}"`))
   .catch(err => console.error("Error connecting to MongoDB", err));
 
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+const User = require('../models/User');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+//Get by Id
+router.get('/details/:userId', (req, res, next) => {
+
+  User.findById(req.params.userId)
+    .then((foundUser) => {
+      console.log("Found user ===>", foundUser)
+      res.json(foundUser)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.json(err)
+    })
+
+})
 
 module.exports = router;
